20013e1dddd549f5fb56bf9a08d56c21a09d6053
Ziya-binziya <binsiyakoya@gmail.com>
2024-08-10 10:20:30 +0530

    first

---
+++ src/Pages/Edit.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { updateUser } from '../../redux/userSlice';
+
+
+
+
+
+
+
+function Edit() {
+  const{id}=useParams()
+
+  const users=useSelector((state)=>state.users)
+  const dispatch=useDispatch()
+  const existingUser=users.filter(a=> a.id == id)
+  const {name,email,contact}=existingUser[0]
+  const[uname,setName]=useState(name)
+  const[uemail,setEmail]=useState(email)
+  const[ucontact,setContact]=useState(contact)
+
+  const navigate=useNavigate();
+
+
+  const handleUpdate=(event)=>{
+    event.preventDefault();
+
+     dispatch(updateUser({id:id,name:uname,email:uemail,contact:ucontact }))
+     navigate('/')
+
+  }
+
+
+  return (
+    <>
+    
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+
+   
+  <div className=' border shadow p-5'>
+  <h4 className='text-center text-primary'>Update User</h4>
+  <form action="" onSubmit={handleUpdate} >
+
+   
+    <label htmlFor="name">Name:</label>
+    <input type="text" name='name' className='form-control' required placeholder='enter name' value={uname} onChange={(e)=>{setName(e.target.value)}}/>
+   
+    <label htmlFor="email">Email:</label>
+    <input type="email" name='email' className='form-control' required placeholder='enter email' value={uemail} onChange={(e)=>{setEmail(e.target.value)}}/>
+   
+    <label htmlFor="contact">Contact:</label>
+    <input type="text" name='name' className='form-control' required placeholder='enter contact' value={ucontact} onChange={(e)=>{setContact(e.target.value)}}/>
+   <br/>
+  <button className='btn btn-primary border shadow'>Update</button>
+  </form>
+
+  </div>
+  </div>
+    </>
+  )
+}
+
+export default Edit
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import Table from 'react-bootstrap/Table';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { dltUser } from '../../redux/userSlice';
+
+
+
+
+
+
+
+
+function Home() {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch()
+
+  const deleteUser = (id) => {
+    dispatch(dltUser({ id: id }))
+  }
+
+  return (
+    <>
+
+        <div className='p-5'>
+      <h4 className='text-primary my-3'>USER DETAILS</h4>
+        
+      <table className='table table-bordered table-dark border shadow'>
+      <thead>
+        <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Contact</th>
+                <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      {
+                users.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.contact}</td>
+                    <td>
+                      <Link to={`/edit/${item.id}`}>
+                        <button className='btn border'><i className="fa-solid fa-pencil"></i></button>
+
+                      </Link>
+                      <button onClick={() => deleteUser(item.id)} className='btn  border ms-1 '><i className="fa-solid fa-trash-can"></i></button>
+                    </td>
+                  </tr>
+
+
+                ))
+              }
+     </tbody>     
+
+    </table>
+    <Link to={'/add'}>
+            <button className='btn btn-primary border text-light '>Create NewUser</button>
+
+          </Link>
+    </div>
+        
+    
+    </>
+  )
+}
+
+export default Home
